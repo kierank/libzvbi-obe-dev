a197bdff3cfb5e32ae78909e5c67a353438a943f
mschimek <mschimek>
2002-01-15 03:24:27 +0000

    Added.

---
+++ test/capture.c
@@ -0,0 +1,540 @@
+/*
+ *  libzvbi test
+ *
+ *  Copyright (C) 2000, 2001, 2002 Michael H. Schimek
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ */
+
+/* $Id: capture.c,v 1.1 2002/01/15 03:24:27 mschimek Exp $ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <assert.h>
+#include <getopt.h>
+
+#include <libzvbi.h>
+
+vbi_capture *		cap;
+vbi_raw_decoder *	par;
+vbi_bool		quit;
+int			src_w, src_h;
+
+int			dump;
+int			dump_ttx;
+int			dump_xds;
+int			dump_cc;
+int			dump_wss;
+int			dump_vps;
+int			dump_sliced;
+int			bin_sliced;
+int			do_read = TRUE;
+int			do_sim;
+
+#include "sim.c"
+
+/*
+ *  Dump
+ */
+
+static inline int
+odd_parity(uint8_t c)
+{
+	c ^= (c >> 4);
+	c ^= (c >> 2);
+	c ^= (c >> 1);
+
+	return c & 1;
+}
+
+extern const int8_t vbi_hamm8val[256];
+extern const uint8_t vbi_bit_reverse[256];
+
+static inline int
+vbi_hamm16(uint8_t *p)
+{
+	return vbi_hamm8val[p[0]] | (vbi_hamm8val[p[1]] << 4);
+}
+
+#define printable(c) ((((c) & 0x7F) < 0x20 || ((c) & 0x7F) > 0x7E) ? '.' : ((c) & 0x7F))
+
+#define PIL(day, mon, hour, min) \
+	(((day) << 15) + ((mon) << 11) + ((hour) << 6) + ((min) << 0))
+
+static void
+dump_pil(int pil)
+{
+	int day, mon, hour, min;
+
+	day = pil >> 15;
+	mon = (pil >> 11) & 0xF;
+	hour = (pil >> 6) & 0x1F;
+	min = pil & 0x3F;
+
+	if (pil == PIL(0, 15, 31, 63))
+		printf(" PDC: Timer-control (no PDC)\n");
+	else if (pil == PIL(0, 15, 30, 63))
+		printf(" PDC: Recording inhibit/terminate\n");
+	else if (pil == PIL(0, 15, 29, 63))
+		printf(" PDC: Interruption\n");
+	else if (pil == PIL(0, 15, 28, 63))
+		printf(" PDC: Continue\n");
+	else if (pil == PIL(31, 15, 31, 63))
+		printf(" PDC: No time\n");
+	else
+		printf(" PDC: %05x, 200X-%02d-%02d %02d:%02d\n",
+			pil, mon, day, hour, min);
+}
+
+static void
+decode_vps(uint8_t *buf)
+{
+	static char pr_label[20];
+	static char label[20];
+	static int l = 0;
+	int cni, pcs, pty, pil;
+	int c;
+
+	if (!dump_vps)
+		return;
+
+	printf("\nVPS:\n");
+
+	c = vbi_bit_reverse[buf[1]];
+
+	if ((char) c < 0) {
+		label[l] = 0;
+		memcpy(pr_label, label, sizeof(pr_label));
+		l = 0;
+	}
+
+	c &= 0x7F;
+
+	label[l] = printable(c);
+
+	l = (l + 1) % 16;
+
+	printf(" 3-10: %02x %02x %02x %02x %02x %02x %02x %02x (\"%s\")\n",
+		buf[0], buf[1], buf[2], buf[3], buf[4], buf[5], buf[6], buf[7], pr_label);
+
+	pcs = buf[2] >> 6;
+
+	cni = + ((buf[10] & 3) << 10)
+	      + ((buf[11] & 0xC0) << 2)
+	      + ((buf[8] & 0xC0) << 0)
+	      + (buf[11] & 0x3F);
+
+	pil = ((buf[8] & 0x3F) << 14) + (buf[9] << 6) + (buf[10] >> 2);
+
+	pty = buf[12];
+
+	printf(" CNI: %04x PCS: %d PTY: %d ", cni, pcs, pty);
+
+	dump_pil(pil);
+}
+
+static void
+decode_ttx(uint8_t *buf, int line)
+{
+	int packet_address;
+	int magazine, packet;
+	int j;
+
+	packet_address = vbi_hamm16(buf + 0);
+
+	if (packet_address < 0)
+		return; /* hamming error */
+
+	magazine = packet_address & 7;
+	packet = packet_address >> 3;
+
+	if (dump_ttx) {
+		printf("WST %x %02d %03d >", magazine, packet, line);
+
+		for (j = 0; j < 42; j++) {
+			char c = printable(buf[j]);
+
+			putchar(c);
+		}
+
+		putchar('<');
+		putchar('\n');
+	}
+}
+
+static void
+decode_xds(uint8_t *buf)
+{
+	if (dump_xds) {
+		char c;
+
+		c = odd_parity(buf[0]) ? buf[0] & 0x7F : '?';
+		c = printable(c);
+		putchar(c);
+		c = odd_parity(buf[1]) ? buf[1] & 0x7F : '?';
+		c = printable(c);
+		putchar(c);
+		fflush(stdout);
+	}
+}
+
+static void
+decode_caption(uint8_t *buf, int line)
+{
+	static vbi_bool xds_transport = FALSE;
+	char c = buf[0] & 0x7F;
+
+	if (line >= 280) { /* field 2 */
+		/* 0x01xx..0x0Exx ASCII_or_NUL[0..32] 0x0Fchks */
+		if (odd_parity(buf[0]) && (c >= 0x01 && c <= 0x0F)) {
+			decode_xds(buf);
+			xds_transport = (c != 0x0F);
+		} else if (xds_transport) {
+			decode_xds(buf);
+		}
+
+		return;
+	}
+
+	if (dump_cc) {
+		c = odd_parity(buf[0]) ? buf[0] & 0x7F : '?';
+		c = printable(c);
+		putchar(c);
+		c = odd_parity(buf[1]) ? buf[1] & 0x7F : '?';
+		c = printable(c);
+		putchar(c);
+		fflush(stdout);
+	}
+}
+
+static void
+decode_wss_625(uint8_t *buf)
+{
+	static const char *formats[] = {
+		"Full format 4:3, 576 lines",
+		"Letterbox 14:9 centre, 504 lines",
+		"Letterbox 14:9 top, 504 lines",
+		"Letterbox 16:9 centre, 430 lines",
+		"Letterbox 16:9 top, 430 lines",
+		"Letterbox > 16:9 centre",
+		"Full format 14:9 centre, 576 lines",
+		"Anamorphic 16:9, 576 lines"
+	};
+	static const char *subtitles[] = {
+		"none",
+		"in active image area",
+		"out of active image area",
+		"?"
+	};
+	int g1 = buf[0] & 15;
+	int parity;
+
+	if (dump_wss) {
+		parity = g1;
+		parity ^= parity >> 2;
+		parity ^= parity >> 1;
+		g1 &= 7;
+
+		printf("WSS PAL: ");
+		if (!(parity & 1))
+			printf("<parity error> ");
+		printf("%s; %s mode; %s colour coding;\n"
+		       "      %s helper; reserved b7=%d; %s\n"
+		       "      open subtitles: %s; %scopyright %s; copying %s\n",
+		       formats[g1],
+		       (buf[0] & 0x10) ? "film" : "camera",
+		       (buf[0] & 0x20) ? "MA/CP" : "standard",
+		       (buf[0] & 0x40) ? "modulated" : "no",
+		       !!(buf[0] & 0x80),
+		       (buf[1] & 0x01) ? "have TTX subtitles; " : "",
+		       subtitles[(buf[1] >> 1) & 3],
+		       (buf[1] & 0x08) ? "surround sound; " : "",
+		       (buf[1] & 0x10) ? "asserted" : "unknown",
+		       (buf[1] & 0x20) ? "restricted" : "not restricted");
+	}
+}
+
+static void
+decode_wss_cpr1204(uint8_t *buf)
+{
+	const int poly = (1 << 6) + (1 << 1) + 1;
+	int g = (buf[0] << 12) + (buf[1] << 4) + buf[2];
+	int j, crc;
+
+	if (dump_wss) {
+		crc = g | (((1 << 6) - 1) << (14 + 6));
+
+		for (j = 14 + 6 - 1; j >= 0; j--) {
+			if (crc & ((1 << 6) << j))
+				crc ^= poly << j;
+		}
+
+		fprintf(stderr, "WSS CPR >> g=%08x crc=%08x\n", g, crc);
+	}
+}
+
+static void
+decode_sliced(vbi_sliced *s, double time, int lines)
+{
+	if (dump_sliced) {
+		vbi_sliced *q = s;
+		int i, j;
+
+		printf("Sliced time: %f\n", time);
+
+		for (i = 0; i < lines; q++, i++) {
+			printf("%04x %3d > ", q->id, q->line);
+
+			for (j = 0; j < sizeof(q->data); j++) {
+				printf("%02x ", (uint8_t) q->data[j]);
+			}
+
+			putchar(' ');
+
+			for (j = 0; j < sizeof(q->data); j++) {
+				char c = printable(q->data[j]);
+				putchar(c);
+			}
+
+			putchar('\n');
+		}
+	}
+
+	for (; lines > 0; s++, lines--) {
+		if (s->id == 0) {
+			continue;
+		} else if (s->id & VBI_SLICED_VPS) {
+			decode_vps(s->data);
+		} else if (s->id & VBI_SLICED_TELETEXT_B) {
+			decode_ttx(s->data, s->line);
+		} else if (s->id & VBI_SLICED_CAPTION_525) {
+			decode_caption(s->data, s->line);
+		} else if (s->id & VBI_SLICED_CAPTION_625) {
+			decode_caption(s->data, s->line);
+		} else if (s->id & VBI_SLICED_WSS_625) {
+			decode_wss_625(s->data);
+		} else if (s->id & VBI_SLICED_WSS_CPR1204) {
+			decode_wss_cpr1204(s->data);
+		} else {
+			fprintf(stderr, "Oops. Unhandled vbi service %08x\n",
+				s->id);
+		}
+	}
+}
+
+/*
+ *  Sliced, binary
+ */
+
+/* hysterical compatibility */
+const int
+services[][2] = {
+	{ VBI_SLICED_TELETEXT_B, 42 },
+	{ VBI_SLICED_CAPTION_625, 2 },
+	{ VBI_SLICED_VPS, 13 },
+	{ VBI_SLICED_WSS_625, 2 },
+	{ VBI_SLICED_WSS_CPR1204, 3 },
+	{ 0, 0 },
+	{ 0, 0 },
+	{ VBI_SLICED_CAPTION_525, 2 }
+};
+
+static void
+binary_sliced(vbi_sliced *s, double time, int lines)
+{
+	static double last = 0.0;
+	int i;
+
+	if (last > 0.0)
+		printf("%f\n%c", time - last, lines);
+	else
+		printf("%f\n%c", 0.04, lines);
+
+	for (; lines > 0; s++, lines--) {
+		for (i = 0; i < 8; i++) {
+			if (s->id & services[i][0]) {
+				printf("%c%c%c", i,
+				       s->line & 0xFF, s->line >> 8);
+				fwrite(s->data, 1, services[i][1], stdout);
+				last = time;
+				break;
+			}
+		}
+	}
+}
+
+static void
+mainloop(void)
+{
+	uint8_t	*raw;
+	vbi_sliced *sliced;
+	int lines;
+	vbi_capture_buffer *raw_buffer;
+	vbi_capture_buffer *sliced_buffer;
+	double timestamp;
+	struct timeval tv;
+
+	if (do_read || do_sim) {
+		assert((raw = malloc(src_w * src_h)));
+		assert((sliced = malloc(sizeof(vbi_sliced) * src_h)));
+	}
+
+	for (quit = FALSE; !quit;) {
+		int r;
+
+		if (do_sim) {
+			read_sim(raw, sliced, &lines, &timestamp);
+			r = 1;
+		} else if (do_read) {
+			r = vbi_capture_read(cap, raw, sliced,
+					     &lines, &timestamp, &tv);
+		} else {
+			r = vbi_capture_pull(cap, &raw_buffer,
+					     &sliced_buffer, &tv);
+		}
+
+		switch (r) {
+		case -1:
+			fprintf(stderr, "VBI read error: %d, %s\n",
+				errno, strerror(errno));
+			exit(EXIT_FAILURE);
+		case 0: 
+			fprintf(stderr, "VBI read timeout\n");
+			exit(EXIT_FAILURE);
+		case 1:
+			break;
+		default:
+			assert(!"reached");
+		}
+
+		if (do_read) {
+			if (dump)
+				decode_sliced(sliced, timestamp, lines);
+			if (bin_sliced)
+				binary_sliced(sliced, timestamp, lines);
+		} else {
+			if (dump)
+				decode_sliced(sliced_buffer->data,
+					      sliced_buffer->timestamp,
+					      sliced_buffer->size
+					      / sizeof(vbi_sliced));
+			if (bin_sliced)
+				binary_sliced(sliced_buffer->data,
+					      sliced_buffer->timestamp,
+					      sliced_buffer->size
+					      / sizeof(vbi_sliced));
+		}
+	}
+}
+
+static const struct option
+long_options[] = {
+	{ "device",	required_argument,	NULL,		'd' },
+	{ "dump-ttx",	no_argument,		&dump_ttx,	TRUE },
+	{ "dump-xds",	no_argument,		&dump_xds,	TRUE },
+	{ "dump-cc",	no_argument,		&dump_cc,	TRUE },
+	{ "dump-wss",	no_argument,		&dump_wss,	TRUE },
+	{ "dump-vps",	no_argument,		&dump_vps,	TRUE },
+	{ "dump-sliced",no_argument,		&dump_sliced,	TRUE },
+	{ "sliced",	no_argument,		&bin_sliced,	TRUE },
+	{ "read",	no_argument,		&do_read,	TRUE },
+	{ "pull",	no_argument,		&do_read,	FALSE },
+	{ "sim",	no_argument,		&do_sim,	TRUE },
+	{ "ntsc",	no_argument,		NULL,		'n' },
+	{ "pal",	no_argument,		NULL,		'p' },
+	{ "verbose",	no_argument,		NULL,		'v' },
+};
+
+int
+main(int argc, char **argv)
+{
+	char *dev_name = "/dev/vbi";
+	char *errstr;
+	unsigned int services;
+	int scanning = 625;
+	vbi_bool verbose = FALSE;
+	int c, index;
+
+	while ((c = getopt_long(argc, argv, "d:npv", long_options, &index)) != -1)
+		switch (c) {
+		case 0: /* set flag */
+			break;
+		case 'd':
+			dev_name = optarg;
+			break;
+		case 'n':
+			scanning = 525;
+			break;
+		case 'p':
+			scanning = 625;
+			break;
+		case 'v':
+			verbose ^= TRUE;
+			break;
+		default:
+			fprintf(stderr, "Unknown option\n");
+			exit(EXIT_FAILURE);
+		}
+
+	dump = dump_ttx | dump_xds | dump_cc
+		| dump_wss | dump_vps | dump_sliced;
+
+	services = VBI_SLICED_VBI_525 | VBI_SLICED_VBI_625
+		| VBI_SLICED_TELETEXT_B | VBI_SLICED_CAPTION_525
+		| VBI_SLICED_CAPTION_625 | VBI_SLICED_VPS
+		| VBI_SLICED_WSS_625 | VBI_SLICED_WSS_CPR1204;
+
+	if (do_sim) {
+		par = init_sim(scanning, services);
+	} else {
+		cap = vbi_capture_v4l2_new(dev_name, /* buffers */ 5,
+			&services, /* strict */ -1, &errstr, /* trace */ verbose);
+
+		if (!cap) {
+			fprintf(stderr, "Cannot capture vbi data "
+				"with v4l2 interface:\n%s\n", errstr);
+
+			if (errstr)
+				free(errstr);
+
+			cap = vbi_capture_v4l_new(dev_name, scanning,
+					  &services, /* strict */ -1, &errstr,
+					  /* trace */ verbose);
+
+			if (!cap) {
+				fprintf(stderr, "Cannot capture vbi data "
+					"with v4l interface:\n%s\n", errstr);
+		
+				exit(EXIT_FAILURE);
+			}
+		}
+
+		assert((par = vbi_capture_parameters(cap)));
+	}
+
+	assert(par->sampling_format == VBI_PIXFMT_YUV420);
+
+	src_w = par->bytes_per_line / 1;
+	src_h = par->count[0] + par->count[1];
+
+	mainloop();
+
+	if (!do_sim)
+		vbi_capture_delete(cap);
+
+	exit(EXIT_SUCCESS);	
+}
---
+++ test/sim.c
@@ -0,0 +1,475 @@
+#include <math.h>
+
+#define AMP 110
+#define DC 60
+
+static vbi_raw_decoder sim;
+static double sim_time;
+
+static inline double
+shape(double ph)
+{
+	double x = sin(ph);
+
+	return x * x;
+}
+
+/*
+ *  Closed Caption Signal Simulator
+ */
+
+static inline double
+cc_sim(double t, double F, unsigned char b1, unsigned char b2)
+{
+	int bits = (b2 << 10) + (b1 << 2) + 2; /* start bit 0 -> 1 */
+	double t1 = 10.5e-6 - .25 / F;
+	double t2 = t1 + 7 / F;		/* CRI 7 cycles */
+	double t3 = t2 + 1.5 / F;
+	double t4 = t3 + 18 / F;	/* 17 bits + raise and fall time */
+	double ph;
+
+	if (t < t1) {
+		return 0.0;
+	} else if (t < t2) {
+		t -= t2;
+		ph = M_PI * 2 * t * F - (M_PI * .5);
+		return sin(ph) / 2 + .5;
+	} else if (t < t3) {
+		return 0.0;
+	} else if (t < t4) {
+		int i, n;
+
+		t -= t3;
+		i = (t * F - .0);
+		n = (bits >> i) & 3; /* low = 0, up, down, high */
+		if (n == 0)
+			return 0.0;
+		else if (n == 3)
+			return 1.0;
+
+		if ((n ^ i) & 1) /* down */
+			ph = M_PI * 2 * (t - 1 / F) * F / 4;
+		else /* up */
+			ph = M_PI * 2 * (t - 0 / F) * F / 4;
+
+		return shape(ph);
+	} else {
+		return 0.0;
+	}
+}
+
+/*
+ *  Wide Screen Signalling Simulator
+ */
+
+static inline double
+wss625_sim(double t, double F, unsigned int bits)
+{
+	static int twobit[] = { 0xE38, 0xE07, 0x1F8, 0x1C7 };
+	static char frame[] =
+		"\0"
+		"\1\1\1\1\1\0\0\0\1\1\1\0\0\0"
+		"\1\1\1\0\0\0\1\1\1\0\0\0\1\1\1"
+		"\0\0\0\1\1\1\1\0\0\0\1\1\1\1\0\0\0\0\0\1\1\1\1\1"
+		"x";
+	double t1 = 11.0e-6 - .5 / F;
+	double t4 = t1 + (29 + 24 + 84) / F;
+	double ph;
+	int i, j, n;
+
+	frame[1 + 29 + 24] = bits & 1;
+
+	if (t < t1) {
+		return 0.0;
+	} else if (t < t4) {
+		t -= t1;
+		i = (t * F - .0);
+		if (i < 29 + 24) {
+			n = frame[i] + 2 * frame[i + 1];
+		} else {
+			j = i - 29 - 24;
+			n = twobit[(bits >> (j / 6)) & 3];
+			n = (n >> (j % 6)) & 3;
+		}
+
+		/* low = 0, down, up, high */
+		if (n == 0)
+			return 0.0;
+		else if (n == 3)
+			return 1.0;
+		if ((n ^ i) & 1) 
+			ph = M_PI * 2 * (t - 1 / F) * F / 4;
+		else /* down */
+			ph = M_PI * 2 * (t - 0 / F) * F / 4;
+
+		return shape(ph);
+	} else {
+		return 0.0;
+	}
+}
+
+static inline double
+wss525_sim(double t, double F, unsigned int bits)
+{
+	double t1 = 11.2e-6 - .5 / F;
+	double t4 = t1 + (2 + 14 + 6 + 1) / F;
+	double ph;
+	int i, n;
+
+	bits = bits * 2 + (2 << 21); /* start bits 10, stop 0 */
+
+	if (t < t1) {
+		return 0.0;
+	} else if (t < t4) {
+		t -= t1;
+		i = (t * F - .0);
+		n = (bits >> (22 - i)) & 3; /* low = 0, up, down, high */
+
+		if (n == 0)
+			return 0.0;
+		else if (n == 3)
+			return 1.0;
+		if ((n ^ i) & 1) 
+			ph = M_PI * 2 * (t - 0 / F) * F / 4;
+		else /* down */
+			ph = M_PI * 2 * (t - 1 / F) * F / 4;
+
+		return shape(ph);
+	} else
+		return 0.0;
+}
+
+/*
+ *  Teletext Signal Simulator
+ */
+
+static inline double
+ttx_sim(double t, double F, const uint8_t *text)
+{
+	double t1 = 10.3e-6 - .5 / F;
+	double t2 = t1 + (45 * 8 + 1) / F; /* 45 bytes + raise and fall time */
+	double ph;
+
+	if (t < t1) {
+		return 0.0;
+	} else if (t < t2) {
+		int i, j, n;
+
+		t -= t1;
+		i = (t * F);
+		j = i >> 3;
+		i &= 7;
+
+		if (j == 0)
+			n = ((text[0] * 2) >> i) & 3;
+		else
+			n = (((text[j - 1] >> 7) + text[j] * 2) >> i) & 3;
+
+		if (n == 0) {
+			return 0.0;
+		} else if (n == 3) {
+			return 1.0;
+		} else if ((n ^ i) & 1) {
+			ph = M_PI * 2 * (t - 1 / F) * F / 4;
+			return shape(ph);
+		} else { /* up */
+			ph = M_PI * 2 * (t - 0 / F) * F / 4;
+			return shape(ph);
+		}
+	} else {
+		return 0.0;
+	}
+
+	if (t < t1) {
+		return 0.0;
+	} else if (t < t2) {
+		int i, j, n;
+
+		t -= t1;
+		i = (t * F - .0);
+		j = i >> 3;
+		if (j < 44)
+			n = ((text[j + 1] * 256 + text[j]) >> i) & 3;
+		else
+			n = (text[i] >> i) & 3;
+
+		return shape(ph);
+	}
+}
+
+static int caption_i = 0;
+static const uint8_t caption_text[] = {
+	0x14, 0x25, 0x14, 0x25, 'L', 'I', 'B', 'Z',
+	'V', 'B', 'I', ' ', 'C', 'A', 'P', 'T',
+	'I', 'O', 'N', ' ', 'S', 'I', 'M', 'U',
+	'L', 'A', 'T', 'I', 'O', 'N', 0x14, 0x2D,
+	0x14, 0x2D /* even size please, add 0 if neccessary */
+};
+
+static inline int
+odd(int c)
+{
+	int n;
+
+	n = c ^ (c >> 4);
+	n = n ^ (n >> 2);
+	n = n ^ (n >> 1);
+
+	if (!(n & 1))
+		c |= 0x80;
+
+	return c;
+}
+
+static uint8_t *
+ttx_next(void)
+{
+	static uint8_t s1[2][10] = {
+		{ 0x02, 0x15, 0x15, 0x15, 0x15, 0x15, 0x15, 0x15, 0x15, 0x15 },
+		{ 0x02, 0x15, 0x02, 0x15, 0x15, 0x15, 0x15, 0x15, 0x15, 0x15 }
+	};
+	static uint8_t s2[32] = "100\2LIBZVBI\7            00:00:00";
+	static uint8_t s3[40] = "  LIBZVBI TELETEXT SIMULATION           ";
+	static uint8_t s4[40] = "  Page 100                              ";
+	static uint8_t s5[10][42] = {
+		{ 0x02, 0x2f, 0x97, 0x20, 0x37, 0x23, 0x23, 0x23, 0x23, 0x23,
+		  0x23, 0x23, 0x23, 0x23, 0x23, 0x23, 0x23, 0x23, 0x23, 0x23,
+		  0x23, 0x23, 0x23, 0x23, 0x23, 0x23, 0x23, 0x23, 0x23, 0x23,
+		  0x23, 0x23, 0x23, 0x23, 0x23, 0x23, 0x23, 0x23, 0x23, 0x23,
+		  0xb5, 0x20 },
+		{ 0xc7, 0x2f, 0x97, 0x0d, 0xb5, 0x04, 0x20, 0x9d, 0x83, 0x8c,
+		  0x08, 0x2a, 0x2a, 0x2a, 0x89, 0x20, 0x20, 0x0d, 0x54, 0x45,
+		  0xd3, 0x54, 0x20, 0xd0, 0xc1, 0xc7, 0x45, 0x8c, 0x20, 0x20,
+		  0x08, 0x2a, 0x2a, 0x2a, 0x89, 0x0d, 0x20, 0x20, 0x1c, 0x97,
+		  0xb5, 0x20 },
+		{ 0x02, 0xd0, 0x97, 0x20, 0xb5, 0x20, 0x20, 0x20, 0x20, 0x20,
+		  0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
+		  0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
+		  0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
+		  0xea, 0x20 },
+		{ 0xc7, 0xd0, 0x97, 0x20, 0xb5, 0x20, 0x20, 0x20, 0x20, 0x20,
+		  0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
+		  0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
+		  0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
+		  0xb5, 0x20 },
+		{ 0x02, 0xc7, 0x97, 0x20, 0xb5, 0x20, 0x20, 0x20, 0x20, 0x20,
+		  0x20, 0x15, 0x1a, 0x2c, 0x2c, 0x2c, 0x2c, 0x2c, 0x2c, 0x2c,
+		  0x2c, 0x2c, 0x2c, 0x2c, 0x2c, 0x2c, 0x2c, 0x2c, 0x2c, 0x2c,
+		  0x2c, 0x2c, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x97, 0x19,
+		  0xb5, 0x20 },
+		{ 0xc7, 0xc7, 0x97, 0x20, 0xb5, 0x20, 0x20, 0x20, 0x20, 0x20,
+		  0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
+		  0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
+		  0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20, 0x20,
+		  0xb5, 0x20 },
+		{ 0x02, 0x8c, 0x97, 0x9e, 0x7f, 0x7f, 0x7f, 0x7f, 0x7f, 0x13,
+		  0x7f, 0x7f, 0x7f, 0x7f, 0x16, 0x7f, 0x7f, 0x7f, 0x7f, 0x92,
+		  0x7f, 0x92, 0x7f, 0x7f, 0x15, 0x7f, 0x7f, 0x15, 0x7f, 0x91,
+		  0x91, 0x7f, 0x7f, 0x91, 0x94, 0x7f, 0x94, 0x7f, 0x94, 0x97,
+		  0xb5, 0x20 },
+		{ 0xc7, 0x8c, 0x97, 0x9e, 0x7f, 0x7f, 0x7f, 0x7f, 0x7f, 0x13,
+		  0x7f, 0x7f, 0x7f, 0x7f, 0x16, 0x7f, 0x7f, 0x7f, 0x7f, 0x92,
+		  0x7f, 0x7f, 0x7f, 0x7f, 0x15, 0x7f, 0x7f, 0x7f, 0x7f, 0x91,
+		  0x7f, 0x7f, 0x7f, 0x7f, 0x94, 0x7f, 0x7f, 0x7f, 0x7f, 0x97,
+		  0xb5, 0x20 },
+		{ 0x02, 0x9b, 0x97, 0x9e, 0x7f, 0x7f, 0x7f, 0x7f, 0x7f, 0x13,
+		  0x7f, 0x7f, 0x7f, 0x7f, 0x16, 0x7f, 0x7f, 0x7f, 0x7f, 0x92,
+		  0x7f, 0x7f, 0x7f, 0x7f, 0x15, 0x7f, 0x7f, 0x7f, 0x7f, 0x91,
+		  0x7f, 0x7f, 0x7f, 0x7f, 0x94, 0x7f, 0x7f, 0x7f, 0x7f, 0x97,
+		  0xb5, 0x20 },
+		{ 0xc7, 0x9b, 0x97, 0x20, 0x23, 0x23, 0x23, 0x23, 0x23, 0x23,
+		  0x23, 0x23, 0x23, 0x23, 0x23, 0x23, 0x23, 0x23, 0x23, 0x23,
+		  0x23, 0x23, 0x23, 0x23, 0x23, 0x23, 0x23, 0x23, 0x23, 0x23,
+		  0x23, 0x23, 0x23, 0x23, 0x23, 0x23, 0x23, 0x23, 0x23, 0x23,
+		  0xa1, 0x20 }
+	};
+	static uint8_t buf[45];
+	static int row = 0, page = 0;
+	int i;
+
+	buf[0] = 0x55;
+	buf[1] = 0x55;
+	buf[2] = 0x27;
+
+	if (row == 0) {
+		memcpy(buf + 3, s1[page], 10);
+		page ^= 1;
+		for (i = 0; i < 32; i++)
+			buf[13 + i] = odd(s2[i]);
+	} else if (row == 1) {
+		buf[3] = 0x02; buf[4] = 0x02;
+		for (i = 0; i < 40; i++)
+			buf[5 + i] = odd(s3[i]);
+	} else if (row == 2) {
+		buf[3] = 0x02; buf[4] = 0x49;
+		for (i = 0; i < 40; i++)
+			buf[5 + i] = odd(s4[i]);
+	} else {
+		memcpy(buf + 3, s5[row - 3], 42);
+	}
+
+	if (++row >= 13) row = 0;
+
+	return buf;
+}
+
+static void
+read_sim(uint8_t *raw_data, vbi_sliced *sliced_data,
+	 int *lines, double *timestamp)
+{
+	uint8_t *buf;
+	double start, inc;
+	int i;
+
+	memset(raw_data, 0, (sim.count[0] + sim.count[1])
+			    * sim.bytes_per_line);
+
+	*timestamp = sim_time;
+
+	if (sim.scanning == 525)
+		sim_time += 1001 / 30000.0;
+	else
+		sim_time += 1 / 25.0;
+
+	start = sim.offset / (double) sim.sampling_rate;
+	inc = 1 / (double) sim.sampling_rate;
+
+	if (sim.scanning == 525) {
+		/* Closed Caption */
+		{
+			buf = raw_data + (21 - sim.start[0])
+				* sim.bytes_per_line;
+
+			for (i = 0; i < sim.bytes_per_line; i++)
+				buf[i] = cc_sim(start + i * inc, 15734 * 32,
+						odd(caption_text[caption_i]),
+						odd(caption_text[caption_i + 1]))
+					* AMP + DC;
+
+			if ((caption_i += 2) > sizeof(caption_text))
+				caption_i = 0;
+		}
+
+		/* WSS NTSC-Japan */
+		{
+			const int poly = (1 << 6) + (1 << 1) + 1;
+			int b0 = 1, b1 = 1;
+			int bits = (b0 << 13) + (b1 << 12);
+			int crc, j;
+
+			crc = (((1 << 6) - 1) << (14 + 6)) + (bits << 6);
+
+			for (j = 14 + 6 - 1; j >= 0; j--) {
+				if (crc & ((1 << 6) << j))
+					crc ^= poly << j;
+			}
+
+			bits <<= 6;
+			bits |= crc;
+
+			/* fprintf(stderr, "WSS CPR << %08x\n", bits); */
+
+			buf = raw_data + (20 - sim.start[0])
+				* sim.bytes_per_line;
+
+			for (i = 0; i < sim.bytes_per_line; i++)
+				buf[i] = wss525_sim(start + i * inc,
+						    447443, bits)
+					* AMP + DC;
+		}
+	} else {
+		/* Closed Caption */
+		{
+			buf = raw_data + (22 - sim.start[0])
+				* sim.bytes_per_line;
+
+			for (i = 0; i < sim.bytes_per_line; i++)
+				buf[i] = cc_sim(start + i * inc, 15625 * 32,
+						odd(caption_text[caption_i]),
+						odd(caption_text[caption_i + 1]))
+					* AMP + DC;
+
+			if ((caption_i += 2) > sizeof(caption_text))
+				caption_i = 0;
+		}
+
+		/* WSS PAL */
+		{
+			int g0 = 1, g1 = 2, g2 = 3, g3 = 4;
+			int bits = (g3 << 11) + (g2 << 8) + (g1 << 4) + g0;
+
+			buf = raw_data + (23 - sim.start[0])
+				* sim.bytes_per_line;
+
+			for (i = 0; i < sim.bytes_per_line; i++)
+				buf[i] = wss625_sim(start + i * inc, 15625 * 320,
+						    bits) * AMP + DC;
+		}
+
+		/* Teletext */
+		{
+			int line, count;
+			uint8_t *text;
+
+			buf = raw_data;
+
+			for (line = sim.start[0], count = sim.count[0];
+			     count > 0; line++, count--, buf += sim.bytes_per_line)
+				if ((line >= 7 && line <= 15)
+				    || (line >= 19 && line <= 21)) {
+					text = ttx_next();
+					for (i = 0; i < sim.bytes_per_line; i++) {
+						buf[i] = ttx_sim(start + i * inc,
+								 15625 * 444,
+								 text) * AMP + DC;
+					}
+				}
+			for (line = sim.start[1], count = sim.count[1];
+			     count > 0; line++, count--, buf += sim.bytes_per_line)
+				if ((line >= 320 && line <= 328)
+				    || (line >= 332 && line <= 335)) {
+					text = ttx_next();
+					for (i = 0; i < sim.bytes_per_line; i++) {
+						buf[i] = ttx_sim(start + i * inc,
+								 15625 * 444,
+								 text) * AMP + DC;
+					}
+				}
+		}
+	}
+
+	*lines = vbi_raw_decode(&sim, raw_data, sliced_data);
+}
+
+static vbi_raw_decoder *
+init_sim(int scanning, unsigned int services)
+{
+	vbi_raw_decoder_init(&sim);
+
+	sim.scanning = scanning;
+	sim.sampling_format = VBI_PIXFMT_YUV420;
+	sim.sampling_rate = 2 * 13500000;
+	sim.bytes_per_line = 1440;
+	sim.offset = 9.7e-6 * sim.sampling_rate;
+	sim.interlaced = FALSE;
+	sim.synchronous = TRUE;
+
+	if (scanning == 525) {
+		sim.start[0] = 10;
+		sim.count[0] = 21 - 10 + 1;
+		sim.start[1] = 272;
+		sim.count[1] = 285 - 272 + 1;
+	} else if (scanning == 625) {
+		sim.start[0] = 6;
+		sim.count[0] = 23 - 6 + 1;
+		sim.start[1] = 318;
+		sim.count[1] = 335 - 318 + 1;
+	} else
+		assert(!"invalid scanning value");
+
+	sim_time = 0.0;
+
+	vbi_raw_decoder_add_services(&sim, services, 0);
+
+	return &sim;
+}
