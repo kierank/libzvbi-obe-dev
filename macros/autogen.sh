689e4fe5a9ccc91e63ccbf73818fe1cd884d7b0f
mschimek <mschimek>
2002-05-23 03:58:30 +0000

    Updated.

--- macros/autogen.sh
+++ macros/autogen.sh
@@ -1,80 +1,64 @@
 #!/bin/sh
 # Run this to generate all the initial makefiles, etc.
 
+PACKAGE=libzvbi
 DIE=0
-
-if [ -n "$GNOME2_PATH" ]; then
-	ACLOCAL_FLAGS="-I $GNOME2_PATH/share/aclocal $ACLOCAL_FLAGS"
-	PATH="$GNOME2_PATH/bin:$PATH"
-	export PATH
-fi
 
 (autoconf --version) < /dev/null > /dev/null 2>&1 || {
   echo
-  echo "**Error**: You must have \`autoconf' installed to compile Gnome."
+  echo "**Error**: You must have 'autoconf' installed to compile $PACKAGE."
   echo "Download the appropriate package for your distribution,"
   echo "or get the source tarball at ftp://ftp.gnu.org/pub/gnu/"
   DIE=1
 }
 
-(grep "^AM_PROG_XML_I18N_TOOLS" $srcdir/configure.in >/dev/null) && {
-  (xml-i18n-toolize --version) < /dev/null > /dev/null 2>&1 || {
-    echo 
-    echo "**Error**: You must have \`xml-i18n-toolize' installed to compile Gnome."
-    echo "Get ftp://ftp.gnome.org/pub/GNOME/stable/sources/xml-i18n-tools/xml-i18n-tools-0.6.tar.gz"
-    echo "(or a newer version if it is available)"
-    DIE=1
-  }
-}
-
 (grep "^AM_PROG_LIBTOOL" $srcdir/configure.in >/dev/null) && {
   (libtool --version) < /dev/null > /dev/null 2>&1 || {
     echo
-    echo "**Error**: You must have \`libtool' installed to compile Gnome."
-    echo "Get ftp://ftp.gnu.org/pub/gnu/libtool-1.2d.tar.gz"
+    echo "**Error**: You must have 'libtool' installed to compile $PACKAGE."
+    echo "Get ftp://ftp.gnu.org/pub/gnu/libtool-1.4d.tar.gz"
     echo "(or a newer version if it is available)"
     DIE=1
   }
 }
 
-#grep "^AM_GNU_GETTEXT" $srcdir/configure.in >/dev/null && {
-#  grep "sed.*POTFILES" $srcdir/configure.in >/dev/null || \
-#  (gettext --version) < /dev/null > /dev/null 2>&1 || {
-#    echo
-#    echo "**Error**: You must have \`gettext' installed to compile Gnome."
-#    echo "Get ftp://alpha.gnu.org/gnu/gettext-0.10.35.tar.gz"
-#    echo "(or a newer version if it is available)"
-#    DIE=1
-#  }
-#}
+grep "^AM_GNU_GETTEXT" $srcdir/configure.in >/dev/null && {
+  grep "sed.*POTFILES" $srcdir/configure.in >/dev/null || \
+  (gettext --version) < /dev/null > /dev/null 2>&1 || {
+    echo
+    echo "**Error**: You must have 'gettext' installed to compile $PACKAGE."
+    echo "Get ftp://alpha.gnu.org/gnu/gettext-0.10.36.tar.gz"
+    echo "(or a newer version if it is available)"
+    DIE=1
+  }
+}
 
-#grep "^AM_GNOME_GETTEXT" $srcdir/configure.in >/dev/null && {
-#  grep "sed.*POTFILES" $srcdir/configure.in >/dev/null || \
-#  (gettext --version) < /dev/null > /dev/null 2>&1 || {
-#    echo
-#    echo "**Error**: You must have \`gettext' installed to compile Gnome."
-#    echo "Get ftp://alpha.gnu.org/gnu/gettext-0.10.35.tar.gz"
-#    echo "(or a newer version if it is available)"
-#    DIE=1
-#  }
-#}
+grep "^AM_GNOME_GETTEXT" $srcdir/configure.in >/dev/null && {
+  grep "sed.*POTFILES" $srcdir/configure.in >/dev/null || \
+  (gettext --version) < /dev/null > /dev/null 2>&1 || {
+    echo
+    echo "**Error**: You must have 'gettext' installed to compile $PACKAGE."
+    echo "Get ftp://alpha.gnu.org/gnu/gettext-0.10.36.tar.gz"
+    echo "(or a newer version if it is available)"
+    DIE=1
+  }
+}
 
 (automake --version) < /dev/null > /dev/null 2>&1 || {
   echo
-  echo "**Error**: You must have \`automake' installed to compile Gnome."
-  echo "Get ftp://ftp.gnu.org/pub/gnu/automake-1.3.tar.gz"
+  echo "**Error**: You must have 'automake' installed to compile $PACKAGE."
+  echo "Get ftp://ftp.gnu.org/pub/gnu/automake-1.6.tar.gz"
   echo "(or a newer version if it is available)"
   DIE=1
   NO_AUTOMAKE=yes
 }
 
-
 # if no automake, don't bother testing for aclocal
 test -n "$NO_AUTOMAKE" || (aclocal --version) < /dev/null > /dev/null 2>&1 || {
   echo
-  echo "**Error**: Missing \`aclocal'.  The version of \`automake'"
+  echo "**Error**: Missing 'aclocal'.  The version of 'automake'"
   echo "installed doesn't appear recent enough."
-  echo "Get ftp://ftp.gnu.org/pub/gnu/automake-1.3.tar.gz"
+  echo "Get ftp://ftp.gnu.org/pub/gnu/automake-1.6.tar.gz"
   echo "(or a newer version if it is available)"
   DIE=1
 }
@@ -84,9 +68,9 @@ if test "$DIE" -eq 1; then
 fi
 
 if test -z "$*"; then
-  echo "**Warning**: I am going to run \`configure' with no arguments."
+  echo "**Warning**: I am going to run 'configure' with no arguments."
   echo "If you wish to pass any to it, please specify them on the"
-  echo \`$0\'" command line."
+  echo $0 "command line."
   echo
 fi
 
