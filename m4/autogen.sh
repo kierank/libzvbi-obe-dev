85d36107f33c8e26f9e4e06e103da4c52c767850
mschimek <mschimek>
2004-10-04 20:47:51 +0000

    Updated to recognize newer automake.

--- m4/autogen.sh
+++ m4/autogen.sh
@@ -2,7 +2,7 @@
 # Based on autogen.sh from gnome-common.
 # Run this to generate all the initial makefiles, etc.
 
-REQUIRED_AUTOCONF_VERSION=2.53
+REQUIRED_AUTOCONF_VERSION=2.57
 REQUIRED_AUTOMAKE_VERSION=1.6
 REQUIRED_LIBTOOL_VERSION=1.4
 REQUIRED_GETTEXT_VERSION=0.11
@@ -103,9 +103,11 @@ AUTOHEADER=`echo $AUTOCONF | sed s/autoconf/autoheader/`
 
 case $REQUIRED_AUTOMAKE_VERSION in
     1.4*) automake_progs="automake-1.4" ;;
-    1.5*) automake_progs="automake-1.7 automake-1.6 automake-1.5" ;;
-    1.6*) automake_progs="automake-1.7 automake-1.6" ;;
-    1.7*) automake_progs="automake-1.7" ;;
+    1.5*) automake_progs="automake-1.5 automake-1.6 automake-1.7 automake-1.8 automake-1.9" ;;
+    1.6*) automake_progs="automake-1.6 automake-1.7 automake-1.8 automake-1.9" ;;
+    1.7*) automake_progs="automake-1.7 automake-1.8 automake-1.9" ;;
+    1.8*) automake_progs="automake-1.8 automake-1.9" ;;
+    1.9*) automake_progs="automake-1.9" ;;
 esac
 version_check automake AUTOMAKE "$automake_progs" $REQUIRED_AUTOMAKE_VERSION \
     "http://ftp.gnu.org/pub/gnu/automake/automake-$REQUIRED_AUTOMAKE_VERSION.tar.gz" || DIE=1
