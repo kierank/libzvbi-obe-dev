672e290d55cd81defb0d585a1219ffcd531945b6
mschimek <mschimek>
2005-01-10 15:49:41 +0000

    Changed strict param from -1 to 0 for proper WSS reception (requires programming of sampling params).

--- test/osc.c
+++ test/osc.c
@@ -19,7 +19,7 @@
  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
-/* $Id: osc.c,v 1.16 2004/12/31 06:05:20 mschimek Exp $ */
+/* $Id: osc.c,v 1.17 2005/01/10 15:49:41 mschimek Exp $ */
 
 #undef NDEBUG
 
@@ -634,6 +634,7 @@ main(int argc, char **argv)
 	char *errstr;
 	unsigned int services;
 	int scanning = 625;
+	int strict;
 	int verbose = 0;
 	int interface = 0;
 	int c, index;
@@ -679,6 +680,8 @@ main(int argc, char **argv)
 		| VBI_SLICED_CAPTION_625 | VBI_SLICED_VPS
 		| VBI_SLICED_WSS_625 | VBI_SLICED_WSS_CPR1204;
 
+	strict = 0;
+
 	if (do_sim) {
 		par = init_sim (scanning, services);
 	} else {
@@ -687,7 +690,7 @@ main(int argc, char **argv)
 				cap = vbi_capture_v4l2k_new
 					(dev_name, /* fd */ -1,
 					 /* buffers */ 5, &services,
-					 /* strict */ -1, &errstr,
+					 strict, &errstr,
 					 /* trace */ !!verbose);
 
 				if (cap)
@@ -702,7 +705,7 @@ main(int argc, char **argv)
 				cap = vbi_capture_v4l2_new (dev_name,
 							    /* buffers */ 5,
 							    &services,
-							    /* strict */ -1,
+							    strict,
 							    &errstr,
 							    /* trace */
 							    !!verbose);
@@ -719,7 +722,7 @@ main(int argc, char **argv)
 				cap = vbi_capture_v4l_new (dev_name,
 							   scanning,
 							   &services,
-							   /* strict */ -1,
+							   strict,
 							   &errstr,
 							   /* trace */
 							   !!verbose);
@@ -737,7 +740,7 @@ main(int argc, char **argv)
 				cap = vbi_capture_bktr_new (dev_name,
 							    scanning,
 							    &services,
-							    /* strict */ -1,
+							    strict,
 							    &errstr,
 							    /* trace */
 							    !!verbose);
