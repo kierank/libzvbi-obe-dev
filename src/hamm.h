41133e3919d52d0bdae64701dfbd6fed14a42152
mschimek <mschimek>
2005-01-19 04:21:17 +0000

    Updated from branch 0.3, parity and Hamming routines are public now.

--- src/hamm.h
+++ src/hamm.h
@@ -1,7 +1,7 @@
 /*
  *  libzvbi - Error correction functions
  *
- *  Copyright (C) 2001 Michael H. Schimek
+ *  Copyright (C) 2001, 2002, 2003, 2004 Michael H. Schimek
  *
  *  Based on code from AleVT 1.5.1
  *  Copyright (C) 1998, 1999 Edgar Toernig <froese@gmx.de>
@@ -21,103 +21,189 @@
  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
-/* $Id: hamm.h,v 1.4 2002/07/16 00:11:36 mschimek Exp $ */
+/* $Id: hamm.h,v 1.5 2005/01/19 04:21:28 mschimek Exp $ */
 
-#ifndef HAMM_H
-#define HAMM_H
+#ifndef __ZVBI_HAMM_H__
+#define __ZVBI_HAMM_H__
 
-#include <inttypes.h>
+#include <inttypes.h>		/* uintN_t */
+#include "misc.h"
+/* #include "macros.h" */
 
-extern const uint8_t		vbi_bit_reverse[256];
-extern const int8_t		vbi_hamm24par[3][256];
-extern const int8_t		vbi_hamm8val[256];
+VBI_BEGIN_DECLS
+
+/* Public */
+
+extern const uint8_t		_vbi_bit_reverse [256];
+extern const uint8_t		_vbi_hamm8_fwd [16];
+extern const int8_t		_vbi_hamm8_inv [256];
+extern const int8_t		_vbi_hamm24_inv_par [3][256];
 
 /**
- * @internal
+ * @addtogroup Error Error correction functions
+ * @ingroup Raw
+ * @brief Helper functions to decode sliced VBI data.
+ * @{
+ */
+
+/**
+ * @param c Unsigned byte.
+ * 
+ * Reverses the bits of the argument.
+ * 
+ * @returns
+ * Data bits 0 [msb] ... 7 [lsb].
+ */
+vbi_inline unsigned int
+vbi_rev8			(unsigned int		c)
+{
+	return _vbi_bit_reverse[(uint8_t) c];
+}
+
+/**
+ * @param c Unsigned 16 bit word.
+ * 
+ * Reverses the bits of the argument.
+ * 
+ * @returns
+ * Data bits 0 [msb] ... 15 [lsb].
+ */
+vbi_inline unsigned int
+vbi_rev16			(unsigned int		c)
+{
+	return _vbi_bit_reverse[(uint8_t) c] * 256
+		+ _vbi_bit_reverse[(uint8_t)(c >> 8)];
+}
+
+/**
+ * @param p Pointer to a 16 bit word, last significant
+ *   byte first.
+ * 
+ * Reverses the bits of the argument.
+ * 
+ * @returns
+ * Data bits 0 [msb] ... 15 [lsb].
+ */
+vbi_inline unsigned int
+vbi_rev16p			(const uint8_t *	p)
+{
+	return _vbi_bit_reverse[p[0]] * 256
+		+ _vbi_bit_reverse[p[1]];
+}
+
+/**
+ * @param c Unsigned byte.
+ *
+ * @returns
+ * Changes the most significant bit of the byte
+ * to make the number of set bits odd.
+ */
+vbi_inline unsigned int
+vbi_par8			(unsigned int		c)
+{
+	c &= 255;
+
+	/* if 0 == (inv_par[] & 32) change bit 7 of c. */
+	c ^= 128 & ~(_vbi_hamm24_inv_par[0][c] << 2);
+
+	return c;
+}
+
+/**
  * @param c Unsigned byte. 
  * 
- * @return
- * If the byte has odd parity (sum of bits mod 2 is 1) the
- * byte AND 127, otherwise -1.
+ * @returns
+ * If the byte has odd parity (sum of bits modulo 2 is 1) the
+ * byte AND 127, otherwise a negative value.
  */
-static inline int
-vbi_parity(unsigned int c)
+vbi_inline int
+vbi_unpar8			(unsigned int		c)
 {
-#if 0 /* #cpu (i686) */
-	int r = c;
+#ifdef __GNUC__
+#if #cpu (i686)
+	int r = c & 127;
 
-	/* This saves cache business and a branch */
-	asm (" andl	$127,%0\n"
-	     " testb	%1,%1\n"
-	     " cmovp    %2,%0\n"
-	     : "+&r" (r) : "abcd" (c), "rm" (-1));
-
+	/* This saves cache flushes and an explicit branch. */
+	__asm__ (" testb	%1,%1\n"
+		 " cmovp	%2,%0\n"
+		 : "+&a" (r) : "c" (c), "rm" (-1));
 	return r;
-#else
-	if (vbi_hamm24par[0][c] & 32)
-		return c & 0x7F;
-	else
-		return -1;
 #endif
+#endif
+	if (_vbi_hamm24_inv_par[0][(uint8_t) c] & 32) {
+		return c & 127;
+	} else {
+		/* The idea is to OR results together to find a parity
+		   error in a sequence, rather than a test and branch on
+		   each byte. */
+		return -1;
+	}
 }
 
-static inline int
-vbi_chk_parity(uint8_t *p, int n)
+extern void
+vbi_par				(uint8_t *		p,
+				 unsigned int		n);
+extern int
+vbi_unpar			(uint8_t *		p,
+				 unsigned int		n);
+
+/**
+ * @param c Integer between 0 ... 15.
+ * 
+ * Encodes a nibble with Hamming 8/4 protection
+ * as specified in EN 300 706, Section 8.2.
+ * 
+ * @returns
+ * Hamming encoded unsigned byte, lsb first transmitted.
+ */
+vbi_inline unsigned int
+vbi_ham8			(unsigned int		c)
 {
-	unsigned int c;
-	int err;
-
-	for (err = 0; n--; p++)
-		if (vbi_hamm24par[0][c = *p] & 32)
-			*p = c & 0x7F;
-		else
-			*p = 0x20, err++;
-
-	return err == 0;
-}
-
-static inline void
-vbi_set_parity(uint8_t *p, int n)
-{
-	unsigned int c;
-
-	for (; n--; p++)
-		if (!(vbi_hamm24par[0][c = *p] & 32))
-			*p = c | 0x80;
+	return _vbi_hamm8_fwd[c & 15];
 }
 
 /**
- * @internal
- * @param c A Hamming 8/4 protected byte, lsb first
- *   transmitted.
+ * @param c Hamming 8/4 protected byte, lsb first transmitted.
  * 
- * This function decodes a Hamming 8/4 protected byte
- * as specified in ETS 300 706 8.2.
+ * Decodes a Hamming 8/4 protected byte
+ * as specified in EN 300 706, Section 8.2.
  * 
- * @return
- * Data bits (D4 [msb] ... D1 [lsb])
- * or -1 if the byte contained incorrectable errors.
+ * @returns
+ * Data bits (D4 [msb] ... D1 [lsb]) or a negative
+ * value if the byte contained incorrectable errors.
  */
-#define vbi_hamm8(c) vbi_hamm8val[(uint8_t)(c)]
-
-/**
- * @internal
- * @param p Pointer to a Hamming 8/4 protected byte pair,
- *   bytes in transmission order, lsb first transmitted.
- * 
- * This function decodes a Hamming 8/4 protected byte pair
- * as specified in ETS 300 706 8.2.
- * 
- * @return
- * Data bits D4 [msb] ... D1 of byte 1, D4 ... D1 [lsb] of byte 2
- * or a negative value if the pair contained incorrectable errors.
- */
-static inline int
-vbi_hamm16(uint8_t *p)
+vbi_inline unsigned int
+vbi_unham8			(unsigned int		c)
 {
-	return vbi_hamm8val[p[0]] | (vbi_hamm8val[p[1]] << 4);
+	return _vbi_hamm8_inv[(uint8_t) c];
 }
 
-extern int vbi_hamm24(uint8_t *p);
+/**
+ * @param p Pointer to a Hamming 8/4 protected 16 bit word,
+ *   last significant byte first, lsb first transmitted.
+ * 
+ * Decodes a Hamming 8/4 protected byte pair
+ * as specified in EN 300 706, Section 8.2.
+ * 
+ * @returns
+ * Data bits D4 [msb] ... D1 of first byte and D4 ... D1 [lsb]
+ * of second byte, or a negative value if any of the bytes
+ * contained incorrectable errors.
+ */
+vbi_inline int
+vbi_unham16p			(const uint8_t *	p)
+{
+	return ((int) _vbi_hamm8_inv[p[0]])
+	  | (((int) _vbi_hamm8_inv[p[1]]) << 4);
+}
 
-#endif /* HAMM_H */
+extern int
+vbi_unham24p			(const uint8_t *	p) vbi_pure;
+
+/** @} */
+
+/* Private */
+
+VBI_END_DECLS
+
+#endif /* __ZVBI_HAMM_H__ */
