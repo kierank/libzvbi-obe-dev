26ac5d6072649436fcca18966ca6e1f2e1869ecf
mschimek <mschimek>
2006-05-22 09:06:10 +0000

    Resynched with 0.3 branch. vbi_log stuff now public. Replaced log level by log mask.

--- src/macros.h
+++ src/macros.h
@@ -1,7 +1,7 @@
 /*
  *  libzvbi - Useful macros
  *
- *  Copyright (C) 2002-2004 Michael H. Schimek
+ *  Copyright (C) 2002, 2003, 2004 Michael H. Schimek
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
@@ -18,7 +18,7 @@
  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
-/* $Id: macros.h,v 1.2 2006/05/07 20:55:24 mschimek Exp $ */
+/* $Id: macros.h,v 1.3 2006/05/22 09:06:10 mschimek Exp $ */
 
 #ifndef __ZVBI_MACROS_H__
 #define __ZVBI_MACROS_H__
@@ -32,6 +32,8 @@
 #endif
 
 VBI_BEGIN_DECLS
+
+/* Public */
 
 #if __GNUC__ >= 4
 #  define _vbi_sentinel sentinel(0)
@@ -60,6 +62,73 @@ VBI_BEGIN_DECLS
 #  define vbi_inline static
 #  define __attribute__(args...)
 #endif
+
+/**
+ * @ingroup Basic
+ * @name Boolean type
+ * @{
+ */
+#ifndef TRUE
+#  define TRUE 1
+#endif
+#ifndef FALSE
+#  define FALSE 0
+#endif
+
+typedef int vbi_bool;
+/** @} */
+
+#ifndef NULL
+#  ifdef __cplusplus
+#    define NULL (0L)
+#  else
+#    define NULL ((void *) 0)
+#  endif
+#endif
+
+/* XXX Document me - for variadic funcs. */
+#define VBI_END ((void *) 0)
+
+#if 0
+typedef void
+vbi_lock_fn			(void *			user_data);
+typedef void
+vbi_unlock_fn			(void *			user_data);
+#endif
+
+typedef enum {
+	/** External error causes, for example lack of memory. */
+	VBI_LOG_ERROR		= 1 << 3,
+
+	/**
+	 * Invalid parameters and similar problems which suggest
+	 * a bug in the caller.
+	 */
+	VBI_LOG_WARNING	= 1 << 4,
+
+	/**
+	 * Causes of possibly undesired results, for example when a
+	 * data service cannot be decoded with the current video
+	 * standard.
+	 */
+	VBI_LOG_NOTICE		= 1 << 5,
+
+	/** Progress messages. */
+	VBI_LOG_INFO		= 1 << 6,
+
+	/** Information useful to debug the library. */
+	VBI_LOG_DEBUG		= 1 << 7,
+} vbi_log_mask;
+
+typedef void
+vbi_log_fn			(vbi_log_mask		level,
+				 const char *		context,
+				 const char *		message,
+				 void *			user_data);
+
+extern vbi_log_fn		vbi_log_on_stderr;
+
+/* Private */
 
 VBI_END_DECLS
 
