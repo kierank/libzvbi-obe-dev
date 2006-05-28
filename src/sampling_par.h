24dee93e2cae8618291949ffe684680dc096d070
mschimek <mschimek>
2006-05-28 20:16:36 +0000

    Don't make the sampling_par functions public yet, have to brush up the definition of video standards first.

--- src/sampling_par.h
+++ src/sampling_par.h
@@ -5,13 +5,16 @@
 
 /* Public */
 
+typedef vbi_raw_decoder vbi_sampling_par;
+
+/* Private */
+
 #define VBI_VIDEOSTD_SET_EMPTY 0
 #define VBI_VIDEOSTD_SET_PAL_BG 1
 #define VBI_VIDEOSTD_SET_625_50 1
 #define VBI_VIDEOSTD_SET_525_60 2
 #define VBI_VIDEOSTD_SET_ALL 3
 typedef uint64_t vbi_videostd_set;
-typedef vbi_raw_decoder vbi_sampling_par;
 
 extern vbi_service_set
 vbi_sampling_par_from_services (vbi_sampling_par *    sp,
@@ -27,8 +30,6 @@ vbi_sampling_par_check_services
 
 extern vbi_videostd_set
 _vbi_videostd_set_from_scanning	(int			scanning);
-
-/* Private */
 
 extern vbi_service_set
 _vbi_sampling_par_from_services_log
