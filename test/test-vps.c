5715383eaaa0e01bebd2465e950ff5d36dd9e300
mschimek <mschimek>
2007-08-31 15:32:22 +0000

    Added GPLv2+ blurb.

--- test/test-vps.c
+++ test/test-vps.c
@@ -1,15 +1,27 @@
 /*
- *  libzvbi test
+ *  libzvbi - VPS low level functions unit test
+ *
  *  Copyright (C) 2006 Michael H. Schimek
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
-/* $Id: test-vps.c,v 1.3 2006/10/08 06:19:48 mschimek Exp $ */
-
-#ifdef HAVE_CONFIG_H
-#  include "config.h"
-#endif
+/* $Id: test-vps.c,v 1.4 2007/08/31 15:32:22 mschimek Exp $ */
 
 #undef NDEBUG
+
 #include <assert.h>
 #include <stdlib.h>
 #include <string.h>
