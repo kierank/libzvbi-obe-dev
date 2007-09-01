29fa778e1ff2dcb9efc6727781d1a0a110952214
mschimek <mschimek>
2007-09-01 01:45:33 +0000

    Make vbi_videostd_set public for dvb_mux.

--- src/sampling_par.h
+++ src/sampling_par.h
@@ -7,14 +7,14 @@
 
 typedef vbi_raw_decoder vbi_sampling_par;
 
-/* Private */
-
 #define VBI_VIDEOSTD_SET_EMPTY 0
 #define VBI_VIDEOSTD_SET_PAL_BG 1
 #define VBI_VIDEOSTD_SET_625_50 1
 #define VBI_VIDEOSTD_SET_525_60 2
 #define VBI_VIDEOSTD_SET_ALL 3
 typedef uint64_t vbi_videostd_set;
+
+/* Private */
 
 extern vbi_service_set
 vbi_sampling_par_from_services (vbi_sampling_par *    sp,
