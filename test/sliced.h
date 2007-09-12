b405bee895880b997e5acce176f3a0e2c7e46c1c
mschimek <mschimek>
2007-09-12 15:52:43 +0000

    Integrated capture and file output functions, added support for raw capturing and raw VBI files. More helper functions.

--- test/sliced.h
+++ test/sliced.h
@@ -18,7 +18,7 @@
  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
-/* $Id: sliced.h,v 1.8 2007/09/01 15:06:55 mschimek Exp $ */
+/* $Id: sliced.h,v 1.9 2007/09/12 15:52:43 mschimek Exp $ */
 
 /* For libzvbi version 0.2.x / 0.3.x. */
 
@@ -28,6 +28,9 @@
 
 #include "src/macros.h"
 #include "src/sliced.h"
+#include "src/sampling_par.h"
+#include "src/bit_slicer.h"
+#include "src/version.h"
 
 /* Reader and write for old test/capture --sliced output.
    Attn: this code is not reentrant. */
@@ -66,14 +69,25 @@ open_sliced_read		(FILE *			fp);
 
 enum file_format {
 	FILE_FORMAT_SLICED = 1,
+	FILE_FORMAT_RAW,
 	FILE_FORMAT_XML,
 	FILE_FORMAT_DVB_PES,
 	FILE_FORMAT_DVB_TS
 };
 
+enum interface {
+	INTERFACE_SIM		= (1 << 0),
+	INTERFACE_DVB		= (1 << 1),
+	INTERFACE_V4L2		= (1 << 2),
+	INTERFACE_V4L		= (1 << 3),
+	INTERFACE_BKTR		= (1 << 4),
+};
+
 typedef vbi_bool
 stream_callback_fn		(const vbi_sliced *	sliced,
 				 unsigned int		n_lines,
+				 const uint8_t *	raw,
+				 const vbi_sampling_par *sp,
 				 double			sample_time,
 				 int64_t		stream_time);
 
@@ -84,8 +98,9 @@ extern char *			program_invocation_name;
 extern char *			program_invocation_short_name;
 #endif
 
-extern vbi_bool			option_quiet;
-extern unsigned long		option_ts_pid;
+extern const char *		option_dev_name;
+extern unsigned int		option_dvb_pid;
+extern vbi_bool		option_quiet;
 extern unsigned int		option_log_mask;
 
 extern void
@@ -100,16 +115,96 @@ error_exit			(const char *		template,
 extern void
 write_error_exit		(const char *		msg);
 extern void
-no_mem_exit			(void);
-extern vbi_bool
-read_stream_loop		(struct stream *	st);
+read_error_exit			(const char *		msg);
 extern void
-read_stream_delete		(struct stream *	st);
-struct stream *
+no_mem_exit			(void);
+
+extern void
+stream_delete			(struct stream *	st);
+
+extern vbi_bool
+stream_loop			(struct stream *	st);
+extern vbi_bool
+write_stream_raw		(struct stream *	st,
+				 uint8_t *		raw,
+				 vbi_sampling_par *	sp,
+				 double			sample_time,
+				 int64_t		stream_time);
+extern vbi_bool
+write_stream_sliced		(struct stream *	st,
+				 const vbi_sliced *	sliced,
+				 unsigned int		n_lines,
+				 const uint8_t *	raw,
+				 const vbi_sampling_par *sp,
+				 double			sample_time,
+				 int64_t		stream_time);
+extern void
+write_stream_set_data_identifier
+				(struct stream *	st,
+				 unsigned int		data_identifier);
+extern void
+write_stream_set_pes_packet_size
+				(struct stream *	st,
+				 unsigned int		min,
+				 unsigned int		max);
+extern struct stream *
+write_stream_new		(enum file_format	file_format,
+				 unsigned int		ts_pid,
+				 unsigned int		system);
+
+extern struct stream *
 read_stream_new			(enum file_format	file_format,
+				 unsigned int		ts_pid,
+				 stream_callback_fn *	callback);
+
+#if 2 == VBI_VERSION_MINOR
+
+typedef struct {
+} vbi_bit_slicer_point;
+
+#endif
+
+extern void
+capture_stream_sim_decode_raw	(struct stream *	st,
+				 vbi_bool		enable);
+extern vbi_bool
+capture_stream_sim_load_caption	(struct stream *	st,
+				 const char *		stream,
+				 vbi_bool		append);
+extern vbi_bool
+capture_stream_get_point	(struct stream *	st,
+				 vbi_bit_slicer_point *point,
+				 unsigned int		row,
+				 unsigned int		nth_bit);
+extern vbi_bool
+capture_stream_collect_points	(struct stream *	st,
+				 vbi_bool		enable);
+extern void
+capture_stream_get_sampling_par	(struct stream *	st,
+				 vbi_sampling_par *	sp);
+extern struct stream *
+capture_stream_new		(unsigned int		interfaces,
+				 const char *		dev_name,
+				 unsigned int		system,
+				 vbi_service_set	services,
+				 unsigned int		n_buffers,
+				 unsigned int		ts_pid,
+				 vbi_bool		sim_interlaced,
+				 vbi_bool		sim_synchronous,
+				 vbi_bool		capture_raw_data,
+				 vbi_bool		read_not_pull,
+				 unsigned int		strict,
 				 stream_callback_fn *	callback);
 extern void
+parse_option_verbose		(void);
+extern void
+parse_option_quiet		(void);
+extern unsigned int
 parse_option_ts			(void);
+extern void
+parse_option_dvb_pid		(void);
+extern void
+parse_option_dev_name		(void);
 extern void
 init_helpers			(int			argc,
 				 char **		argv);
