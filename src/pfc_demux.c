8fa80022665cdac145d3ad9858317ac27af7fadc
mschimek <mschimek>
2007-11-27 17:41:16 +0000

    Changed license from GPLv2 to GPLv2-or-later and updated the FSF address. Doxumentation fixes.

--- src/pfc_demux.c
+++ src/pfc_demux.c
@@ -4,8 +4,9 @@
  *  Copyright (C) 2003-2004 Michael H. Schimek
  *
  *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License version 2 as
- *  published by the Free Software Foundation.
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
  *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -14,10 +15,10 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 
-/* $Id: pfc_demux.c,v 1.8 2007/11/05 19:53:07 mschimek Exp $ */
+/* $Id: pfc_demux.c,v 1.9 2007/11/27 17:41:16 mschimek Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #  include "config.h"
@@ -29,13 +30,6 @@
 
 #define BLOCK_SEPARATOR 0x0C
 #define FILLER_BYTE 0x03
-
-/**
- * @addtogroup PFCDemux Teletext Page Function Clear demultiplexer
- * @ingroup LowDec
- * @brief Separating data transmitted in Page Function Clear
- *   Teletext packets (ETS 300 708 section 4).
- */
 
 /** @internal */
 void
@@ -395,7 +389,7 @@ vbi_pfc_demux_delete		(vbi_pfc_demux *	dx)
 /**
  * @param pgno Page to take PFC data from.
  * @param stream PFC stream to be demultiplexed.
- * @param callback Function to be called by vbi_pfc_demux_demux() when
+ * @param callback Function to be called by vbi_pfc_demux_feed() when
  *   a new data block is available.
  * @param user_data User pointer passed through to @a cb function.
  *
