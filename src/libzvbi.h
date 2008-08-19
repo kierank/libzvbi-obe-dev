6c066afb6dd52c5c725a44c927a6003bea8424b7
mschimek <mschimek>
2008-08-19 10:04:36 +0000

    *** empty log message ***

--- src/libzvbi.h
+++ src/libzvbi.h
@@ -29,7 +29,7 @@
 
 #define VBI_VERSION_MAJOR 0
 #define VBI_VERSION_MINOR 2
-#define VBI_VERSION_MICRO 30
+#define VBI_VERSION_MICRO 32
 
 #ifdef __cplusplus
 extern "C" {
--- src/raw_decoder.c
+++ src/raw_decoder.c
@@ -19,7 +19,7 @@
  *  Boston, MA  02110-1301  USA.
  */
 
-/* $Id: raw_decoder.c,v 1.23 2008/02/19 00:35:21 mschimek Exp $ */
+/* $Id: raw_decoder.c,v 1.24 2008/08/19 10:04:46 mschimek Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #  include "config.h"
@@ -499,6 +499,8 @@ decode_pattern			(vbi3_raw_decoder *	rd,
 
 			/* Positive match, output decoded line. */
 
+			/* FIXME: if we have a field number we should
+			   really only set the service id of one field. */
 			sliced->id = job->id;
 			sliced->line = 0;
 
