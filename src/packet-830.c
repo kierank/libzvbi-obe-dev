101c7461b2355176722f99d94eb0131f93f8208b
mschimek <mschimek>
2009-03-04 21:48:43 +0000

    Updated the doxumentation and added a brief explanation of Packet 8/30. Replaced a vbi_bcd2bin() call because the function has insufficient precision in libzvbi 0.2.

--- src/packet-830.c
+++ src/packet-830.c
@@ -17,7 +17,7 @@
  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
-/* $Id: packet-830.c,v 1.2 2009/02/16 13:40:24 mschimek Exp $ */
+/* $Id: packet-830.c,v 1.3 2009/03/04 21:48:43 mschimek Exp $ */
 
 #ifdef HAVE_CONFIG_H
 #  include "config.h"
@@ -26,19 +26,36 @@
 #include <assert.h>
 #include <errno.h>
 #include "misc.h"
-#include "bcd.h"		/* vbi_bcd2bin() */
+#include "bcd.h"		/* vbi_lbcd2bin() */
 #include "hamm.h"		/* vbi_rev16p(), vbi_iham8() */
 #include "packet-830.h"
 
 /**
  * @addtogroup Packet830 Teletext Packet 8/30 Decoder
  * @ingroup LowDec
- * @brief Functions to decode Teletext packets 8/30 (ETS 300 706).
+ * @brief Functions to decode Teletext packet 8/30 (ETS 300 706).
+ *
+ * Teletext pages are transmitted in packets numbered 0 to 31. Packet
+ * 0 to 25 contain the text of the page, packet 26 to 29 additional
+ * information like Fastext links. Packet 30 and 31 are reserved for
+ * data transmissions unrelated to any page. Since each packet
+ * contains a magazine number 1 to 8 (the first digit of the Teletext
+ * page number) 16 logical channels can be distinguished. Packet 30
+ * with magazine number 8 carries a Country and Network Identifier,
+ * and either a local time (format 1) or PDC label (format 2).
+ *
+ * These are low level functions. See test/decode.c for a usage
+ * example.
+ *
+ * The @a vbi_decoder module can decode a full Teletext signal and
+ * provide the information transmitted in packet 8/30 as @c
+ * VBI_EVENT_NETWORK, @c VBI_EVENT_NETWORK_ID, @c VBI_EVENT_LOCAL_TIME
+ * and @c VBI_EVENT_PROG_ID. See examples/network.c and
+ * examples/pdc1.c.
  */
 
-/*
-  Resources:
-  http://pdc.ro.nu/jd-code.html
+/* Resources:
+   http://pdc.ro.nu/jd-code.html
 */
 
 /**
@@ -46,13 +63,16 @@
  * @param buffer Teletext packet as defined for @c VBI_SLICED_TELETEXT_B,
  *   i.e. 42 bytes without clock run-in and framing code.
  *
- * Decodes a Teletext packet 8/30 format 1 according to
- * ETS 300 706 section 9.8.1, returning the 16 bit Country and
- * Network Identifier in @a cni.
+ * Decodes a Teletext packet 8/30 format 1 according to ETS 300 706
+ * section 9.8.1, returning the contained 16 bit Country and Network
+ * Identifier in @a *cni.
  *
  * @returns
  * Always @c TRUE, no error checking possible. It may be prudent to
- * wait until two identical CNIs have been received.
+ * wait for a second transmission of the received CNI to ensure
+ * correct reception.
+ *
+ * @since 0.2.34
  */
 vbi_bool
 vbi_decode_teletext_8301_cni	(unsigned int *		cni,
@@ -66,24 +86,45 @@ vbi_decode_teletext_8301_cni	(unsigned int *		cni,
 	return TRUE;
 }
 
+/* Should use vbi_bcd2bin() but that function is limited to four BCD
+   digits in libzvbi 0.2 and cannot be changed for compatibility
+   reasons. */
+static int
+bcd2bin				(int			bcd)
+{
+	unsigned int f;
+	int bin = 0;
+
+	for (f = 1; f < 100000; f = f * 10) {
+		bin += (bcd & 15) * f;
+		bcd >>= 4;
+	}
+
+	return bin;
+}
+
 /**
- * @param time The time in seconds since Epoch (1970-01-01 00:00 UTC)
+ * @param time The current time in seconds since 1970-01-01 00:00 UTC
  *   will be stored here.
- * @param seconds_east The time zone offset in seconds east of UTC is
- *   stored here, including daylight saving time. To get the local
- *   time of the intended audience add @a seconds_east to @a time.
+ * @param seconds_east The offset of the local time of the intended
+ *   audience of the program in seconds east of UTC will be stored here,
+ *   including a daylight-saving time offset if daylight-saving is
+ *   currently in effect in that time zone. To get the local time of the
+ *   intended audience add @a seconds_east to @a time.
  * @param buffer Teletext packet as defined for @c VBI_SLICED_TELETEXT_B,
  *   i.e. 42 bytes without clock run-in and framing code.
  * 
- * Decodes a Teletext packet 8/30 format 1 according to
- * ETS 300 706 section 9.8.1, returning the current time in the UTC
- * time zone and the time zone of the intended audience.
+ * Decodes a Teletext packet 8/30 format 1 according to ETS 300 706
+ * section 9.8.1, returning the current time in the UTC time zone and
+ * the time zone of the intended audience of the program.
  *
  * @returns
  * On error the function returns @c FALSE:
  * - The buffer contains incorrectable errors or
  * - The time is not representable as a time_t.
- * In these cases @a time and @a seconds_east remain unchanged.
+ * In these cases @a *time and @a *seconds_east remain unchanged.
+ *
+ * @since 0.2.34
  */
 vbi_bool
 vbi_decode_teletext_8301_local_time
@@ -113,7 +154,7 @@ vbi_decode_teletext_8301_local_time
 		return FALSE;
 	}
 
-	mjd = vbi_bcd2bin (bcd);
+	mjd = bcd2bin (bcd);
 
 	/* UTC time. */
 
@@ -153,12 +194,11 @@ vbi_decode_teletext_8301_local_time
 		offset = -offset;
 
 	t = (mjd - 40587) * 86400 + utc;
-#if 3 == VBI_VERSION_MINOR
 	if (t < TIME_MIN || t > TIME_MAX) {
 		errno = EOVERFLOW;
 		return FALSE;
 	}
-#endif
+
 
 	*time = t;
 
@@ -172,13 +212,15 @@ vbi_decode_teletext_8301_local_time
  * @param buffer Teletext packet as defined for @c VBI_SLICED_TELETEXT_B,
  *   i.e. 42 bytes without clock run-in and framing code.
  *
- * Decodes a Teletext packet 8/30 format 2 according to
- * ETS 300 706 section 9.8.2, returning the 16 bit Country and
- * Network Identifier in @a cni.
+ * Decodes a Teletext packet 8/30 format 2 according to ETS 300 706
+ * section 9.8.2, returning the contained 16 bit Country and Network
+ * Identifier in @a *cni.
  *
  * @returns
  * @c FALSE if the buffer contains incorrectable errors. In this case
- * @a cni remains unchanged.
+ * @a *cni remains unchanged.
+ *
+ * @since 0.2.34
  */
 vbi_bool
 vbi_decode_teletext_8302_cni	(unsigned int *		cni,
@@ -212,16 +254,18 @@ vbi_decode_teletext_8302_cni	(unsigned int *		cni,
 }
 
 /**
- * @param pid PDC data will be stored here.
+ * @param pid PDC program ID will be stored here.
  * @param buffer Teletext packet as defined for @c VBI_SLICED_TELETEXT_B,
  *   i.e. 42 bytes without clock run-in and framing code.
  * 
- * Decodes a Teletext packet 8/30 format 2 according to
- * ETS 300 231, storing PDC recording-control data in @a pid.
+ * Decodes a Teletext packet 8/30 format 2 according to ETS 300 231,
+ * and stores the contained PDC recording-control data in @a *pid.
  *
  * @returns
  * @c FALSE if the buffer contains incorrectable errors or invalid
- * data. In this case @a pid remains unchanged.
+ * data. In this case @a *pid remains unchanged.
+ *
+ * @since 0.2.34
  */
 vbi_bool
 vbi_decode_teletext_8302_pdc	(vbi_program_id *	pid,
@@ -252,9 +296,7 @@ vbi_decode_teletext_8302_pdc	(vbi_program_id *	pid,
 
 	pid->channel	= VBI_PID_CHANNEL_LCI_0 + ((b[6] >> 2) & 3);
 
-#if 3 == VBI_VERSION_MINOR
 	pid->cni_type	= VBI_CNI_TYPE_8302;
-#endif
 
 	pid->cni	= (+ ((b[ 7] & 0x0F) << 12)
 			   + ((b[10] & 0x03) << 10)
