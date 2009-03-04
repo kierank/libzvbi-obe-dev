874102153d0f7bd707dd8fe87027105b53664fc6
mschimek <mschimek>
2009-03-04 21:48:40 +0000

    A few modifications to make Doxygen happy.

--- src/packet-830.h
+++ src/packet-830.h
@@ -17,7 +17,7 @@
  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  */
 
-/* $Id: packet-830.h,v 1.2 2009/02/16 13:40:24 mschimek Exp $ */
+/* $Id: packet-830.h,v 1.3 2009/03/04 21:48:40 mschimek Exp $ */
 
 #ifndef __ZVBI_PACKET_830_H__
 #define __ZVBI_PACKET_830_H__
@@ -29,6 +29,8 @@
 
 VBI_BEGIN_DECLS
 
+/* Public */
+
 /**
  * @addtogroup Packet830
  * @{
@@ -36,22 +38,36 @@ VBI_BEGIN_DECLS
 extern vbi_bool
 vbi_decode_teletext_8301_cni	(unsigned int *		cni,
 				 const uint8_t		buffer[42])
-  _vbi_nonnull ((1, 2));
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
+  _vbi_nonnull ((1, 2))
+#endif
+  ;
 extern vbi_bool
 vbi_decode_teletext_8301_local_time
 				(time_t *		utc_time,
 				 int *			seconds_east,
 				 const uint8_t		buffer[42])
-  _vbi_nonnull ((1, 2, 3));
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
+  _vbi_nonnull ((1, 2, 3))
+#endif
+  ;
 extern vbi_bool
 vbi_decode_teletext_8302_cni	(unsigned int *		cni,
 				 const uint8_t		buffer[42])
-  _vbi_nonnull ((1, 2));
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
+  _vbi_nonnull ((1, 2))
+#endif
+  ;
 extern vbi_bool
 vbi_decode_teletext_8302_pdc	(vbi_program_id *	pid,
 				 const uint8_t		buffer[42])
-  _vbi_nonnull ((1, 2));
+#ifndef DOXYGEN_SHOULD_SKIP_THIS
+  _vbi_nonnull ((1, 2))
+#endif
+  ;
 /** @} */
+
+/* Private */
 
 VBI_END_DECLS
 
